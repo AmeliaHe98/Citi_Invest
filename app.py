98d7d2b02ea8e70d92e5074e116440866e583786
Amelia He <amelia.he98@gmail.com>
2020-08-05 11:47:39 -0700

    flask

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template
+import config
+
+app = Flask(__name__)
+app.config.from_object(config)
+
+
+class Person(object):
+    name = ''
+    age = 0
+
+
+class Expense(object):
+    pass
+
+
+class Investment(object):
+    pass
+
+
+class Income(object):
+    pass
+
+
+class Account(object):
+    pass
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/expenses/')
+def expense():
+    return render_template('expenses.html')
+
+
+@app.route('/income/')
+def income():
+    return render_template('income.html')
+
+
+if __name__ == '__main__':
+    app.run()
