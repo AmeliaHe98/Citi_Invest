614520bbab8e4cf9583c3fcba8e7255dc2a54eda
llh121 <43103168+llh121@users.noreply.github.com>
2020-08-05 23:46:13 -0700

    add models, resources, db.py and others related to db

--- app.py
+++ app.py
@@ -1,31 +1,62 @@
-from flask import Flask, render_template
-from flask_restful import Resource, Api
-import config
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_restful import Api
 from flask_jwt import JWT
 
-from expense import ExpenseList
-from income import IncomeList
 from security import authenticate, identity
-from user import UserRegister
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'jose'
 api = Api(app)
-app.secret_key = "sprint4"
-jwt = JWT(app, authenticate, identity)
-
-api.add_resource(IncomeList, '/income')
-api.add_resource(ExpenseList, '/expenses')
-api.add_resource(UserRegister, '/login')
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 
-@app.route('/login/')
-def login():
-    return render_template('sign-in.html')
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+jwt = JWT(app, authenticate, identity)  # /auth
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    app.run()
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
+# from flask import Flask, render_template
+# from flask_restful import Api
+# from flask_jwt import JWT
+# from expense import ExpenseList
+# from income import IncomeList
+# from security import authenticate, identity
+# from resources.user import UserRegister
+#
+# app = Flask(__name__)
+# api = Api(app)
+# app.secret_key = "sprint4"
+# jwt = JWT(app, authenticate, identity)
+#
+# api.add_resource(IncomeList, '/income')
+# api.add_resource(ExpenseList, '/expenses')
+# api.add_resource(UserRegister, '/login')
+#
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+#
+#
+# @app.route('/login/')
+# def login():
+#     return render_template('sign-in.html')
+#
+# if __name__ == '__main__':
+#     app.run()
