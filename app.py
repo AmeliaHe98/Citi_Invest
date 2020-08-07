f23007e0a48286cbcff64db7f1fcdd314084e028
cc-21 <55864124+cc-21@users.noreply.github.com>
2020-08-06 23:00:13 -0400

    Update the files

--- app.py
+++ app.py
@@ -1,74 +1,50 @@
 from flask import Flask, render_template
-from flask_restful import Api
-from flask_jwt import JWT
-
-from security import authenticate, identity
-from resources.user import UserRegister
-# from resources.item import Item, ItemList
-# from resources.store import Store, StoreList
+import config
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = 'jose'
-api = Api(app)
+app.config.from_object(config)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+class Person(object):
+    name = ''
+    age = 0
 
 
-# <<<<<<< HEAD
-# jwt = JWT(app, authenticate, identity)  # /auth
-#
-# api.add_resource(Store, '/store/<string:name>')
-# api.add_resource(StoreList, '/stores')
-# api.add_resource(Item, '/item/<string:name>')
-# api.add_resource(ItemList, '/items')
-# api.add_resource(UserRegister, '/register')
-#
-# =======
+class Expense(object):
+    pass
+
+
+class Investment(object):
+    pass
+
+
+class Income(object):
+    pass
+
+
+class Account(object):
+    pass
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
 @app.route('/login/')
 def login():
-    return render_template('signin.html')
-@app.route('/expense/')
+    return render_template('sign-in.html')
+
+
+@app.route('/expenses/')
 def expense():
     return render_template('expenses.html')
+
+
 @app.route('/income/')
 def income():
     return render_template('income.html')
 
+
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
-    app.run(port=5000, debug=True)
-# from flask import Flask, render_template
-# from flask_restful import Api
-# from flask_jwt import JWT
-# from expense import ExpenseList
-# from income import IncomeList
-# from security import authenticate, identity
-# from resources.user import UserRegister
-#
-# app = Flask(__name__)
-# api = Api(app)
-# app.secret_key = "sprint4"
-# jwt = JWT(app, authenticate, identity)
-#
-# api.add_resource(IncomeList, '/income')
-# api.add_resource(ExpenseList, '/expenses')
-# api.add_resource(UserRegister, '/login')
-#
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-#
-#
-# @app.route('/login/')
-# def login():
-#     return render_template('sign-in.html')
-#
-# if __name__ == '__main__':
-#     app.run()
+    app.run(port=5000)
